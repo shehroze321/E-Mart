b1ba5d1e18933ad7a797e0165f81df518b2c591a
dev2 <Dev2@th-global.co>
2023-09-11 22:38:10 +0500

    E-mart frontend url done

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://e-mart-frontend.vercel.app"],
     credentials: true,
   })
 );
--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -40,7 +40,7 @@ router.post(
 
       const activationToken = createActivationToken(seller);
 
-      const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+      const activationUrl = `https://e-mart-frontend.vercel.app/seller/activation/${activationToken}`;
 
       try {
         await sendMail({
